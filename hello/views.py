73a134a36cc052e7407a31b6ba7d266592dc8c0a
Haikun Huang <haikun.huang@sap.com>
2017-05-03 13:55:38 +0800

    Initial commit

---
+++ hello/views.py
@@ -0,0 +1,36 @@
+import datetime
+
+import simplejson
+import tushare as ts
+import time
+
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def hello(request):
+    s = 'Hello World!'
+    current_time = datetime.datetime.now()
+    html = '<html><head></head><body><h1> %s </h1><p> %s </p></body></html>' % (s, current_time)
+    return HttpResponse(html)
+
+def getHistoryStock(request):
+    id = request.GET.get('id')
+    date = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+    df = ts.get_hist_data(id, start='2015-01-05', end=date)
+    df = df[::-1]
+    df = df.to_json(orient='index')
+    return HttpResponse(simplejson.dumps(df, ensure_ascii=False))
+
+def getRealStock(request):
+    id = request.GET.get('id')
+    df = ts.get_realtime_quotes(id)
+    df = df.to_json(orient='index')
+    return HttpResponse(simplejson.dumps(df, ensure_ascii=False))
+
+def getTodayStock(request):
+    id = request.GET.get('id')
+    df = ts.get_today_ticks(id)
+    df = df[::-1]
+    df = df.to_json(orient='index')
+    return HttpResponse(simplejson.dumps(df, ensure_ascii=False))
